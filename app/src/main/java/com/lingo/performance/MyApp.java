e3def312aa9b8c12abb7dbfed1bba2796a94d71e
牛琳果 <linguo.niu@ada.local>
2020-06-09 12:38:55 +0800

    集成百度地图

--- app/src/main/java/com/lingo/performance/MyApp.java
+++ app/src/main/java/com/lingo/performance/MyApp.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import android.text.TextUtils;
 
+import com.baidu.mapapi.CoordType;
+import com.baidu.mapapi.SDKInitializer;
 import com.tencent.bugly.crashreport.CrashReport;
 import com.umeng.commonsdk.UMConfigure;
 
@@ -55,6 +57,13 @@ public class MyApp extends Application {
 
             }
         });
+
+
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        SDKInitializer.initialize(context);
+        //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
+        //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
+        SDKInitializer.setCoordType(CoordType.BD09LL);
     }
 
     /**
